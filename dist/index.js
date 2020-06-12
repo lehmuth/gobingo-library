60534a50853a12e174b0014f775ca79cb2461306
lehmuth <dominiklehmuth@gmail.com>
2020-06-12 07:49:56 +0200

    fix(rooms): Added EntryRoomMetaData to index.ts

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var metadata_1 = require("./rooms/bingo/metadata");
 Object.defineProperty(exports, "BingoRoomMetaData", { enumerable: true, get: function () { return metadata_1.BingoRoomMetaData; } });
+var metadata_2 = require("./rooms/entry/metadata");
+Object.defineProperty(exports, "EntryRoomMetaData", { enumerable: true, get: function () { return metadata_2.EntryRoomMetaData; } });
