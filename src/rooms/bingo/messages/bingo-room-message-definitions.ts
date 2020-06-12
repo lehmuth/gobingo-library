695e5fbe27760cdf18d5f947b4f89a15d9821851
lehmuth <dominiklehmuth@gmail.com>
2020-06-12 22:36:00 +0200

    feat(rooms): Add message definitions

---
+++ src/rooms/bingo/messages/bingo-room-message-definitions.ts
@@ -0,0 +1,5 @@
+import { MessageDefinition } from "../../../classes/message-definition";
+
+export const SEND_CHAT_MESSAGE: MessageDefinition = {
+    id: "sendChatMessage"
+};
---
+++ src/rooms/entry/messages/entry-room-message-definitions.ts
@@ -0,0 +1,8 @@
+import { MessageDefinition } from "../../../classes/message-definition";
+
+export const SEND_CHAT_MESSAGE: MessageDefinition = {
+    id: "sendChatMessage"
+};
+export const BROADCAST_CHAT_MESSAGE: MessageDefinition = {
+    id: "broadcastChatMessage"
+};
